840688d0b1f6f95910fbe317ea3e28fcf95273a5
Jonathan Jefferies <jonathan.jefferies@supergroup.co.uk>
2014-01-24 12:09:00 +0000

    Added Braking control state.

--- src/ControlState.js
+++ src/ControlState.js
@@ -1,13 +1,15 @@
 
-var ControlState = function(left, right, accelerate) {
+var ControlState = function(left, right, accelerate, brake) {
 	this.__keys = {};
 	this.__keys["key" + left] = "left";
 	this.__keys["key" + right] = "right";
 	this.__keys["key" + accelerate] = "accelerate";
+	this.__keys["key" + brake] = "brake";
 	
 	this.left = false;
 	this.right = false;
 	this.accelerate = false;
+	this.brake = false;
 };
 
 ControlState.prototype.keyDown = function(key) {
