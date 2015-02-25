98661b85a8de96d8c61f25a0728db652b861394e
Jonathan Jefferies <jonathan@jjok.co.uk>
2015-02-25 00:47:49 +0000

    Got car movement working a bit more.

--- src/Car.js
+++ src/Car.js
@@ -14,8 +14,8 @@ var Car = function(box) {
 	 * @var float
 	 */
 	this.__speed = 0;
-	this.__top_speed = 0.5;
-	this.__acceleration = 0.1;
+	this.__top_speed = 0.2;
+	this.__acceleration = 0.001;
 	this.__braking = 0.1;
 	
 	/**
@@ -25,15 +25,15 @@ var Car = function(box) {
 	this.__heading = 0;
 	
 	/**
-	 * -1 - 1
+	 * 
 	 * @var float
 	 */
 	this.__steer_angle = 0;
+//	this.__steer_amount = 0.01;
+	this.__max_steer_angle = 0.03;
 	
-	//TODO
-	this.__length = 1;
-	this.__half_length = this.__length / 2;
-
+//	this.__length = 1;
+//	this.__half_length = this.__length / 2;
 	
 	this.__state = Car.State.REST;
 };
@@ -62,23 +62,23 @@ Car.prototype.brake = function() {
 };
 
 Car.prototype.turnLeft = function() {
-	this.__steer_angle = Math.max(this.__steer_angle - 0.01, -1);
-//	this.__steer_angle = -1;
+//	this.__steer_angle = Math.max(this.__steer_angle - this.__steer_amount, -this.__max_steer_angle);
+	this.__steer_angle = -this.__max_steer_angle;
 };
 
 Car.prototype.turnRight = function() {
-	this.__steer_angle = Math.min(this.__steer_angle + 0.01, 1);
-//	this.__steer_angle = 1;
+//	this.__steer_angle = Math.min(this.__steer_angle + this.__steer_amount, this.__max_steer_angle);
+	this.__steer_angle = this.__max_steer_angle;
 };
 
 Car.prototype.unturn = function() {
-	if(this.__steer_angle > 0) {
-		this.__steer_angle = Math.max(this.__steer_angle - 0.01, 0);
-	}
-	else if(this.__steer_angle < 0) {
-		this.__steer_angle = Math.min(this.__steer_angle + 0.01, 0);
-	}
-//	this.__steer_angle = 0;
+//	if(this.__steer_angle > 0) {
+//		this.__steer_angle = Math.max(this.__steer_angle - 0.01, 0);
+//	}
+//	else if(this.__steer_angle < 0) {
+//		this.__steer_angle = Math.min(this.__steer_angle + 0.01, 0);
+//	}
+	this.__steer_angle = 0;
 };
 
 Car.prototype.update = function(dt) {
@@ -92,46 +92,48 @@ Car.prototype.update = function(dt) {
 
 	if(this.__speed != 0) {
 		
-		//find front wheel position
-		var front_wheel_position = this.__box.position.add(new BABYLON.Vector3(
-			this.__half_length * Math.cos(this.__heading),
-			0,
-			this.__half_length * Math.sin(this.__heading)
-		));
-		//move front wheel in wheel direction
-		front_wheel_position = front_wheel_position.add(new BABYLON.Vector3(
-			this.__speed * dt * Math.cos(this.__steer_angle + this.__heading),
-			this.__half_length,
-			this.__speed * dt * Math.sin(this.__steer_angle + this.__heading)
-		));
-		
-		//find back wheel position
-		var back_wheel_position = this.__box.position.subtract(new BABYLON.Vector3(
-			this.__half_length * Math.cos(this.__heading),
-			0,
-			this.__half_length * Math.sin(this.__heading)
-		));
-		//move back wheel in car direction
-		back_wheel_position = back_wheel_position.add(new BABYLON.Vector3(
-			this.__speed * dt * Math.cos(this.__heading),
-			this.__half_length,
-			this.__speed * dt * Math.sin(this.__heading)
-		));
-		
-		//work out angle between points
-		//rotate car to angle
-		
-		//move car to new center point
+//		//find front wheel position
+//		var front_wheel_position = this.__box.position.add(new BABYLON.Vector3(
+//			this.__half_length * Math.cos(this.__heading),
+//			0,
+//			this.__half_length * Math.sin(this.__heading)
+//		));
+//		//move front wheel in wheel direction
+//		front_wheel_position = front_wheel_position.add(new BABYLON.Vector3(
+//			this.__speed * dt * Math.cos(this.__steer_angle + this.__heading),
+//			this.__half_length,
+//			this.__speed * dt * Math.sin(this.__steer_angle + this.__heading)
+//		));
+//		
+//		//find back wheel position
+//		var back_wheel_position = this.__box.position.subtract(new BABYLON.Vector3(
+//			this.__half_length * Math.cos(this.__heading),
+//			0,
+//			this.__half_length * Math.sin(this.__heading)
+//		));
+//		//move back wheel in car direction
+//		back_wheel_position = back_wheel_position.add(new BABYLON.Vector3(
+//			this.__speed * dt * Math.cos(this.__heading),
+//			this.__half_length,
+//			this.__speed * dt * Math.sin(this.__heading)
+//		));
+//		
+//		//work out angle between points
+//		//rotate car to angle
+//		
+//		//move car to new center point
 //		this.__box.position = new BABYLON.Vector3(
 //			(front_wheel_position.x + back_wheel_position.x) / 2,
 //			this.__half_length,
 //			(front_wheel_position.z + back_wheel_position.z) / 2
 //		);
-		this.__heading = Math.atan2(
-			front_wheel_position.z - back_wheel_position.z,
-			front_wheel_position.x - back_wheel_position.x
-		);
+//		this.__heading = Math.atan2(
+//			front_wheel_position.z - back_wheel_position.z,
+//			front_wheel_position.x - back_wheel_position.x
+//		);
 //		console.log(this.__heading);
+		this.__heading += this.__steer_angle;
+		
 		switch(this.__state) {
 			case Car.State.ACCELERATING:
 				this.__box.applyImpulse(
@@ -142,7 +144,7 @@ Car.prototype.update = function(dt) {
 			
 			case Car.State.BRAKING:
 				this.__box.applyImpulse(
-					new BABYLON.Vector3(-this.__speed * Math.cos(this.__heading), 0.1, -this.__speed * Math.sin(this.__heading)),
+					new BABYLON.Vector3(-3 * Math.cos(this.__heading), 0, -3 * Math.sin(this.__heading)),
 					this.__box.position
 				);
 				break;
